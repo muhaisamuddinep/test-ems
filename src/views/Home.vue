93fea335144a9f4995ced869f9d6c4fa8c7c8235
Muhammad Aisamuddine Eka Pramudita <muhammad.pramudita@iconpln.co.id>
2022-08-04 18:05:48 +0700

    [Isam] EMS - Test Intgrasi EMS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-form @submit="login">
+      <v-row class="pa-6">
+        <v-col cols="6">
+          <v-text-field outlined v-model="username" label="Username" placeholder="Username"/>
+          <v-text-field type="password" outlined v-model="password" label="Password" placeholder="Password"/>
+          <v-btn type="submit" shaped>Login</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+const CryptoJS = require("crypto-js")
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    username: '',
+    password: '',
+  }),
+  methods: {
+    login() {
+      const url = "https://stage-biller-agregator.air.id/web/bot/autologin/" + encodeURIComponent(this.encryptText({username: this.username, password: this.password}))
+      window.open(url, "_blank")
+    },
+    encryptText(plainText) {
+      return CryptoJS.AES.encrypt(JSON.stringify(plainText), "Icon Plus 2022")
+    }
   }
 }
 </script>
